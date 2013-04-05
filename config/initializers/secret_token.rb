85834ecb252f8c39c8ae59e7163f25b248b6d39c
alphang <alphang@server.fake>
2013-04-05 12:52:39 -0700

    Initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uploader::Application.config.secret_token = '9ca9ac2739c58f34c11471a82a6cdcd4a5f2157336786089572a3b1427add2f139eff2f6664273648c40b0412d0ea147a8009407734c7eb981f2c162c294629f'
