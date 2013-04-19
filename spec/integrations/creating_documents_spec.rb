8f211fe964582c09437c16482630f7999919105d
alphang <alphang@server.fake>
2013-04-18 17:38:56 -0700

    Add S3 support. Fixed spec so Capybara can work with external link.

--- spec/integrations/creating_documents_spec.rb
+++ spec/integrations/creating_documents_spec.rb
@@ -6,11 +6,9 @@ feature 'Creating Documents' do
     fill_in :title, with: "Lorem Ipsum"
     attach_file "Attachment", File.expand_path("spec/fixtures/test.txt")
     click_button "Upload Document"
-    #save_and_open_page
     page.should have_content("Document has been created.")
-    page.should have_content("test.txt")
-    click_link "test.txt"
-    page.should have_content("Lorem ipsum dolor sit amet")
+    h = 'http://uploaderalpha.s3.amazonaws.com/documents/assets'
+    page.should have_selector "a[href^='#{h}']", text: "test.txt"
   end
 
   scenario "Creating an invalid document" do
@@ -18,7 +16,6 @@ feature 'Creating Documents' do
     fill_in :title, with: "Lorem Ipsum"
     attach_file "Attachment", File.expand_path("spec/fixtures/Canada.png")
     click_button "Upload Document"
-    save_and_open_page
     page.should_not have_content("Document has been created.")
     page.should_not have_content("Canada.txt")
   end
